a00a40703e06f044e5679e1d08f1dde063e762d4
btorezan <bruno.torezan@gmail.com>
2022-10-05 21:52:24 -0300

    configuração listview  questions

--- meuincoterm2020/lib/screens/info/info_screen.dart
+++ meuincoterm2020/lib/screens/info/info_screen.dart
@@ -5,6 +5,8 @@ import 'package:meuincoterm2020/controllers/incoterms_controller.dart';
 import 'package:meuincoterm2020/models/incoterm.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
+import '../../controllers/question_controller.dart';
+
 class InfoScreen extends StatelessWidget {
   const InfoScreen({super.key, required this.incotermAbbr});
   final String incotermAbbr;
@@ -15,6 +17,8 @@ class InfoScreen extends StatelessWidget {
     TextStyle name = const TextStyle(color: Color(0xff001741), fontSize: 20, fontWeight: FontWeight.bold);
 
     Incoterm incoterm = IncotermsController().getIncotermByAbbr(incotermAbbr);
+    QuestionController qController = QuestionController();
+    var questions = qController.questions;
 
     final infos = incoterm.longText.entries.map((entry) {
       return Column(
@@ -59,6 +63,49 @@ class InfoScreen extends StatelessWidget {
             ]),
             const SizedBox(
               height: 20,
+            ),
+            SizedBox(
+              height: 300,
+              child: Center(
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.all(8),
+                    itemCount: incoterm.answers.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 200,
+                        width: MediaQuery.of(context).size.width / 1.8,
+                        child: Card(
+                          color: incoterm.answers[index] ? Colors.green[200] : Colors.red[200],
+                          child: SizedBox(
+                            width: 300,
+                            height: 100,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AutoSizeText(
+                                    questions[index].questionText.toString(),
+                                    style: const TextStyle(fontSize: 30),
+                                    textAlign: TextAlign.center,
+                                    maxLines: 4,
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  AutoSizeText(
+                                    incoterm.answers[index] ? "SIM" : "NÃO",
+                                    style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
             ),
             Expanded(
               child: SingleChildScrollView(
