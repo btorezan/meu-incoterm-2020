2a40c23ab338daecd375a44fbf8425d7b69e4c4d
btorezan <bruno.torezan@gmail.com>
2022-10-16 08:34:36 -0300

    ajuste no sobre

--- meuincoterm2020/lib/screens/about/about_screen.dart
+++ meuincoterm2020/lib/screens/about/about_screen.dart
@@ -6,70 +6,89 @@ class AboutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextStyle title = TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 25.sp, fontWeight: FontWeight.bold);
-    TextStyle text = TextStyle(color: Theme.of(context).primaryColor, fontSize: 20.sp, fontWeight: FontWeight.bold);
+    TextStyle title = TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 20.sp, fontWeight: FontWeight.bold);
+    TextStyle text = TextStyle(color: Theme.of(context).primaryColor, fontSize: 18.sp, fontWeight: FontWeight.bold);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sobre Nós"),
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Expanded(
-              child: Image.asset(
-                "assets/images/logo.png",
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Image.asset(
+                  "assets/images/logo.png",
+                ),
               ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Ideia e Pesquisa",
-                    style: title,
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "Aline Amaral Libâneo Torezan\nGlaice Cristina da Rosa Franco",
-                    style: text,
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "Design e Desenvolvimento",
-                    style: title,
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "Bruno Reis Torezan",
-                    style: text,
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "Orientador e Co-Orientador",
-                    style: title,
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "Silvio Soares da Rosa\nPaula Rodrigues Granato",
-                    style: text,
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "Agradecimentos",
-                    style: title,
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "FATEC Prof. Antonio Belizandro Barbosa Rezende",
-                    style: text,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+              Expanded(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Ideia e Pesquisa",
+                          style: title,
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "Aline Amaral Libâneo Torezan\nGlaice Cristina da Rosa Franco",
+                          style: text,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Design e Desenvolvimento",
+                          style: title,
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "Bruno Reis Torezan",
+                          style: text,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Orientador e Co-Orientador",
+                          style: title,
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "Prof. Msc. Silvio Soares da Rosa\nProf. Msc. Paula Rodrigues Granato",
+                          style: text,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Agradecimentos",
+                          style: title,
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "FATEC Prof. Antonio Belizandro Barbosa Rezende",
+                          style: text,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
